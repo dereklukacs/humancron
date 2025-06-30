0c356a0fd38dee35c9717a3f032be71c02e627ed
Derek <dlukacs@umich.edu>
2025-06-30 18:57:40 -0500

    Initial Commit

---
+++ humancron/humancronApp.swift
@@ -0,0 +1,17 @@
+//
+//  humancronApp.swift
+//  humancron
+//
+//  Created by obsess on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct humancronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ humancronTests/humancronTests.swift
@@ -0,0 +1,17 @@
+//
+//  humancronTests.swift
+//  humancronTests
+//
+//  Created by obsess on 6/30/25.
+//
+
+import Testing
+@testable import humancron
+
+struct humancronTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
